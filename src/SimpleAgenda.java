a36d32f73db2f68f9483552b27a4218cafbe4c29
jarleen <jarleenmartinez@hotmail.com>
2024-01-24 20:54:29 -0400

    Creacion de estructura de agenda

---
+++ src/SimpleAgenda.java
@@ -0,0 +1,20 @@
+public class SimpleAgenda {
+    public static void main(String[] args) {
+    }
+
+    public static void agregarContacto(String nombre) {
+
+    }
+
+    public static void removerContacto(int id) {
+    }
+
+    public static String actualizarContacto(int posicion) {
+        return "N/A";
+    }
+
+    public static void mostarContactos() {
+    }
+
+
+}
