891f4f2ab3f4c40143926db676a9aac17cce2549
jarleen <jarleenmartinez@hotmail.com>
2024-02-15 19:30:13 -0400

    Actualizacion agrendaContacto

--- src/SimpleAgenda.java
+++ src/SimpleAgenda.java
@@ -1,41 +1,75 @@
 import java.util.Scanner;
 
 public class SimpleAgenda {
-    public static void main(String[] args) {
-      agregarContacto();
-    }
+    public static String[] contactos = new String[10];
 
     public static void agregarContacto() {
-        Scanner scanner = new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
+         String contacto;
         System.out.println("Bienvenido a la agenda ");
+        String aggcontactos = "Ingrese un contacto a arreglar: ";
+        System.out.println(contactos);
+        contacto = sc.nextLine();
 
-        System.out.println("Ingrese un contacto a agregar: ");
-        String variable1 = scanner.nextLine();
-
-        if (variable1.length() >=8 && variable1.length() <=30 ){
-            System.out.println("Contacto agregado con exito");
-        } else {
-            System.out.println("Nombre muy largo");
+        boolean encontrado = false;
+        for (int i = 0; i < contactos.length; i++) {
+            if (contactos[i] != null && contactos[i].equals(contacto)) {
+                encontrado = true;
+                break;
+            }
         }
-
-    }
-
-    public static void removerContacto(int id) {
-        if (id >= 1000 && id <= 9999) {
-            System.out.println("Verificando el contacto a eliminar");
+        if (!encontrado) {
+            for (int i = 0; i < contactos.length; i++) {
+                if (contactos[i] == null) {
+                    contactos[i] = contacto;
+                    System.out.println("Contacto " + contacto + "Agregado satisfactoriamente");
+                    break;
+                }
+            }
         }
         else {
-            System.out.println(" ID invalido");
+            System.out.println("Ya se ha agregado anteriormente el contacto " + contacto);
         }
     }
 
+    public void removerContacto()
+    { Scanner Remove = new Scanner(System.in);
+            System.out.println("Ingrese el nombre del contacto a remover ");
+        String contacto = Remove.nextLine();
 
-    public static String actualizarContacto(int posicion) {
-        return "N/A";
+        boolean encontrado = false;
+        int indice = -1;
+
+        for (int i = 0; i < contactos.length; i++) {
+            if (contactos[i] != null && contactos[i].equals(contacto)) {
+                encontrado = true;
+                indice = i;
+                break;
+            }
     }
 
-    public static void mostarContactos() {
-        System.out.println(" Mostrando contactos... ");
+        if (encontrado) {
+            contactos[indice] = null;
+            System.out.println("El contacto " + contacto + " fue removido exitosamente!");
+        } else {
+            System.out.println("No existe tal contacto: " + contacto);
+        }
+    }
+
+    public static String actualizarContacto (int a) {
+
+        return "N/A";
+    }
+    public static void mostrarContactos() {
+        System.out.println("Mostrando contactos: ");
+        for (String contacto : contactos) {
+            if (contacto != null) {
+                System.out.println(contacto);
+            }
+        }
+    }
+    public static void main(String[] args) {
+
     }
 
 }
