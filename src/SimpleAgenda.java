fea31aa472f9841eb602c2fc8e319a2bc1af4d06
jarleen <jarleenmartinez@hotmail.com>
2024-03-14 23:59:12 -0400

    Practica 5

--- src/SimpleAgenda.java
+++ src/SimpleAgenda.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -5,14 +6,15 @@ import java.io.File;
 
 public class SimpleAgenda {
     public static String[] contactos = new String[10];
+    public static File arch = new File("contactos.txt");
 
-    public static void main(String[] args) {
-        agregarContacto();
+    public static void main(String[] args) throws IOException {
+        mostrarContacto();
 
     }
 
-    public static void generarArchivo () throws IOException {
-        File arch  = new File("contactos.txt");
+    // paso 1.
+    public static void generarArchivo() throws IOException {
         arch.createNewFile();
         FileWriter escribir = new FileWriter(arch);
         escribir.write("contacto,telefono\n" +
@@ -23,77 +25,44 @@ public class SimpleAgenda {
 
                 "Raider,8097410032\n" +
 
-                "Roger,8095554141\n" );
+                "Roger,8095554141\n");
 
         escribir.close();
     }
 
 
-    public static void agregarContacto() {
-         Scanner sc = new Scanner(System.in);
-         String contacto;
-        System.out.println("Bienvenido a la agenda ");
-        String aggcontactos = "Ingrese un contacto a arreglar: ";
-        System.out.println(contactos);
-        contacto = sc.nextLine();
-
+    // paso 2.
+    public static void mostrarContacto() throws FileNotFoundException {
+        Scanner scanner1 = new Scanner(arch);
+        String contacto;
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Ingrese el nombre a consultar");
+        contacto = scanner2.nextLine();
         boolean encontrado = false;
-        for (int i = 0; i < contactos.length; i++) {
-            if (contactos[i] != null && contactos[i].equals(contacto)) {
+        while (scanner1.hasNextLine()) {
+            String linea = scanner1.nextLine();
+            if (linea.startsWith(contacto)) {
                 encontrado = true;
                 break;
             }
         }
-        if (!encontrado) {
-            for (int i = 0; i < contactos.length; i++) {
-                if (contactos[i] == null) {
-                    contactos[i] = contacto;
-                    System.out.println("Contacto " + contacto + "Agregado satisfactoriamente");
-                    break;
-                }
+        if (encontrado) System.out.println("El contacto " + contacto + " existe");
+        else System.out.println("No existen registros de este contacto");
+    }
+
+
+    // paso 3.
+    public static void mostrarContactos() throws FileNotFoundException {
+        System.out.println("mostrando contactos");
+        Scanner scanner = new Scanner(arch);
+        while (scanner.hasNextLine()) {
+            String linea = scanner.nextLine();
+            if (!linea.startsWith("contacto")) {
+                System.out.println(linea);
             }
-        }
-        else {
-            System.out.println("Ya se ha agregado anteriormente el contacto " + contacto);
+
         }
     }
-
-    public void removerContacto()
-    { Scanner Remove = new Scanner(System.in);
-            System.out.println("Ingrese el nombre del contacto a remover ");
-        String contacto = Remove.nextLine();
-
-        boolean encontrado = false;
-        int indice = -1;
-
-        for (int i = 0; i < contactos.length; i++) {
-            if (contactos[i] != null && contactos[i].equals(contacto)) {
-                encontrado = true;
-                indice = i;
-                break;
-            }
-    }
-
-        if (encontrado) {
-            contactos[indice] = null;
-            System.out.println("El contacto " + contacto + " fue removido exitosamente!");
-        } else {
-            System.out.println("No existe tal contacto: " + contacto);
-        }
-    }
-
-    public static String actualizarContacto (int a) {
-
-        return "N/A";
-    }
-    public static void mostrarContactos() {
-        System.out.println("Mostrando contactos: ");
-        for (String contacto : contactos) {
-            if (contacto != null) {
-                System.out.println(contacto);
-            }
-        }
-    }
-
-
 }
+
+
